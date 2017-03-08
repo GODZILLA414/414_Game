106cefc8b410d5faf1f057307b62df7926e4488c
Justin Daza <justinjo@ualberta.ca>
2017-03-07 21:42:16 -0700

    "Started a procedural music generator file, modified aggro414 to also calculate the distance between enemy and player"

---
+++ Research Project/PunchMan Test/PunchMan Test/Assets/OurScripts/Pcd_Mus1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pcd_Mus1 : MonoBehaviour {
+    // Use this for initialization
+    private Aggro414 enemy1;            //Primarily used to get the distance from enemy
+    
+    private static List<char> musicLvl1;        //Music track files will be explicitly called and organized based on 'level'
+    private static List<char> musicLvl2;
+    private static List<char> musicLvl3;
+    private static List<char> musicLvl4;
+    void Start () {
+        enemy1 = GetComponent<Aggro414>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
